f3be758f80457493f51e347dd6cc1dbf2cfeb4ad
guumarques <gustavo.gr697.gm@gmail.com>
2026-02-27 11:27:38 -0300

    updating page

---
+++ script.js
@@ -0,0 +1,16 @@
+function scrollToContent() {
+    window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth'
+    });
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
